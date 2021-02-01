c10df138f736073f9e3171ce4432b78fb2499136
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-01 10:05:51 +0300

    Add files via upload

---
+++ Class Lo.py
@@ -0,0 +1,9 @@
+# __Author__ __Lencof__
+# Create Class Lo
+
+class Lo: # Create Class Lo
+    pass # An empty block
+
+Len = Lo()
+Len = ('Hello World') # Insert your text
+print(Len) # print outputs text
