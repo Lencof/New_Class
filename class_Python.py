c992a56a78100e64caf5d48bf22b17fc8723deed
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-02 15:02:01 +0300

    Create class_Python.py

---
+++ class_Python.py
@@ -0,0 +1,9 @@
+# __Author__ __Lencof__
+# Create class Python
+
+class Python: # Create class Python
+    pass #An empty block
+
+OLI = Python() # appropriation
+OLI = 'Hello Worlds', 'Bye!' # Insert your text
+print(OLI) # print outputs text
