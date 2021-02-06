659688a0527d71d483f63f4cae4a280ea0351190
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-06 09:59:29 +0300

    Update class_Vo.py

--- class_Vo.py
+++ class_Vo.py
@@ -1,1 +1,9 @@
-df
+# __Author__ __Lencof__
+# Create class_Vo.py
+
+class Vo: # Create class_Vo
+  pass # An empty block
+
+BUG = Vo() # assign
+BUG = ('I have') # Insert your text
+print(BUG) # print outputs text
