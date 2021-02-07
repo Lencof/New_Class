38a64be2daac6769ddc71566c59c1bd35f1b11d9
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-07 13:55:34 +0300

    Update class_Vavex.py

--- class_Vavex.py
+++ class_Vavex.py
@@ -3,4 +3,9 @@
 #!/usr/bin/python
 # class_Vavex.py
 
-class
+class Vavex(): # create class Vavex
+  pass # An empty block
+
+Ex = Vavex() # assigment
+Ex = (100 * 8) # your text
+print(Ex) # insert Ex into print
