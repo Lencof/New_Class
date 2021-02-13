117c6b734d27970f0f295234547d74110541a03c
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-13 18:06:43 +0300

    Update class_D.py

--- class_D.py
+++ class_D.py
@@ -1,8 +1,8 @@
 # __Author__ __Lencof__
 # class_D.py
 
-class D():
-    print('Hello Worlds!')
-    print('Hello Worlds!')
-    print('Hello Worlds!')
-D()  
+class D(): # create class_D():
+    print('Hello Worlds!') # your text
+    print('Hello Worlds!') # your text
+    print('Hello Worlds!') # your text
+D() # close D()
