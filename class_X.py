bdb3c52fc8d938d307fabe86bd47d58eaa0ff17e
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-15 14:35:24 +0300

    Update class_X.py

--- class_X.py
+++ class_X.py
@@ -1,3 +1,9 @@
 # __Author__ __Lencof__
 # class_X.py
 
+class X(): # create class X():
+    print('How are you?') # your text
+    print('How are you?') # your text
+    print('How are you?') # your text
+    
+X() # close
