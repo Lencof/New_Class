e2bb7f730c93ff50d0e2380ea3aff822f1ec5a7b
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-27 10:37:17 +0300

    Update class_Payments.py

--- class_Payments.py
+++ class_Payments.py
@@ -1,1 +1,9 @@
-class 
+# __Author__ __Lencof__
+# class_Payments.py
+
+class Payments(): # create class Payments():
+   pass # An empty block
+  
+A = Payments() # assignment
+A = (1000 * 1000) # your text and figures
+print(A) # print (A) outputs 
