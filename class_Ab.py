3371297def7c82ea14e557d9aa6c70067c20c60c
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-03 10:55:30 +0300

    Create class_Ab.py

---
+++ class_Ab.py
@@ -0,0 +1,9 @@
+# __Author__ __Lencof__
+# class_Ab.py
+
+class Ab(): # create class Ab
+    print('Hello Ab') # your text
+    print('Hello Ab') # your text
+    print('Hello Ab') # your text
+    
+Ab() # close()
