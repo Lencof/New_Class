916278f2f20edbbb59202ed9f6df4082a1ece30b
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-05 09:14:27 +0300

    Create class_Pass.py

---
+++ class_Pass.py
@@ -0,0 +1,9 @@
+# __Author__ __Lencof__
+# class_Pass.py
+
+class Pass(): # create class Pass():
+  pass # empty block
+
+C = 100 # your figures
+B = 100 # yout figures
+print(C + B) # print() deduces
