175e55a484a948b0125c268de3d4abbd55fbcf60
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-09 13:18:19 +0300

    Update class_Close.py

--- class_Close.py
+++ class_Close.py
@@ -1,1 +1,10 @@
+# __Author__ __Lencof__
+# class_Close.py
 
+# create class Close():
+class Close():
+    print('Close') # your text
+    print('Close') # your text
+    print('Close') # your text
+    
+Close() # close class
