c3b509582f00379caaf46aead7a4ae0d70a76f7c
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-10 07:41:25 +0300

    Create class_Asma.py

---
+++ class_Asma.py
@@ -0,0 +1,10 @@
+# __Author__ __Lencof__
+# class_Asma.py
+
+class Asma(): # create class Asma():
+    pass # an empty block
+  
+Al = 1000 # your figures
+Dex = 0 # your figures
+
+Al == Dex # compare Al == Dex
