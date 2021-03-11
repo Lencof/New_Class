90433aec97685656ffafba1830158d2235c6610c
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-11 17:58:49 +0300

    Create class_Asax.py

---
+++ class_Asax.py
@@ -0,0 +1,11 @@
+# __Author__ __Lencof__
+# class_Asax.py
+
+class Asax(): # create class Asax():
+  pass # an empty block
+
+
+X = Asax # appropriation
+X = 890 # your figures
+S = 890 # your figures 
+print(X * S) # function displays print()
