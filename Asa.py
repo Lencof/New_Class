f28c43a0a115976f0eabbdab5c327a55a15ebdd1
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-12 08:26:17 +0300

    Create Asa.py

---
+++ Asa.py
@@ -0,0 +1,10 @@
+# __Author__ __Lencof__
+# Asa.py
+
+class Asa(): # create class Asa():
+  pass # an empty block
+
+S = Asa # appropriation
+S = 111 # your figures
+D = 111 # your figures
+print(S + D) # use function print()
