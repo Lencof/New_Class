ec6e6ba38618939519981aacbc538b5119a40d21
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-12 17:06:40 +0300

    Create class_Nation.py

---
+++ class_Nation.py
@@ -0,0 +1,11 @@
+# __Author__ __Lencof__
+# class_Nation.py
+
+class Nation(): # create class Nation():
+    pass # an empty block
+  
+AD = Nation # appropriation
+AP = 1200000000000000000000000000000000000000000000000000000 # your figures
+AD = 1200000000000000000000000000000000000000000000000000000 # your figures
+
+print(AP * AD) # use print()
