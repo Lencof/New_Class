dfb9f65ab05b009596a1f5ae0b358510cfccacf7
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-12 12:49:54 +0300

    Create class_Point.py

---
+++ class_Point.py
@@ -0,0 +1,11 @@
+# __Author__ __Lencof__
+# class_Point.py
+
+class Point(): # create class Point():
+    pass # an empty block
+  
+Q = Point # appropriation
+C = 1999 # your figures
+Q = 2000 # your figures
+
+print(Q > C) # comparison
