68abbbf2f25b15613dd71e064c4d6f6bb16aa495
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-12 08:38:28 +0300

    Create class_Trop.py

---
+++ class_Trop.py
@@ -0,0 +1,10 @@
+# __Author__ __Lencof__
+# class_Trop.py
+
+class Trop(): # create class Trop():
+  pass # an empty block
+
+Al = Trop # appropriation
+Al = (1000 * 1000) # your figures
+Sa = (1000 * 1000) # your figures
+print(Al * Sa) # use function print() 
