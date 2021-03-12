20164c0809ce4eb37488591809a9b48eb5b06fe0
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-12 08:32:07 +0300

    Create class_BUGAT.py

---
+++ class_BUGAT.py
@@ -0,0 +1,10 @@
+# __Author__ __Lencof__
+# class_BUGAT.py
+
+class BUGAT(): # create class BUGAT
+  pass # an empty block
+
+Poli = BUGAR # appropriation
+Poli = (120 + 120) # your figures
+Exma = (90 + 90) # your figures
+print(Poli + Exma) # use function print()
