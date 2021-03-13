d48cfc4800658d038792bad398b87aca2730fa06
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-13 10:27:07 +0300

    Create class_SAPE.py

---
+++ class_SAPE.py
@@ -0,0 +1,9 @@
+# __Author__ __Lencof__
+# class_SAPE.py
+
+class SAPE(): # create class SAPE():
+    pass # an empty block
+  
+AG = 9 # your figures
+for AG in range(0,9): # use range, insert your figures
+    print(AG) # use function print() 
