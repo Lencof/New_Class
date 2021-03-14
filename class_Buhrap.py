d1e78a6918ad4d39f7b4b2dcf8ff82972c2f1e14
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-14 13:34:45 +0300

    Create class_Buhrap.py

---
+++ class_Buhrap.py
@@ -0,0 +1,10 @@
+# __Author__ __Lencof__
+# class_Buhrap.py
+
+class Buhrap(): # create class Buhrap
+    pass # an empty block
+  
+A = Buhrap # appropriation
+A = 123 # your figures
+B = 123 # your figures
+print(A + B) # use function print() 
