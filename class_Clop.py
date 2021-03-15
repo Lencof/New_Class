7ebbc091bd24cb3a52848060d75a8c69306b3e5d
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-15 08:28:26 +0300

    Create class_Clop.py

---
+++ class_Clop.py
@@ -0,0 +1,10 @@
+# __Author__ __Lencof__
+# class_Clop.py
+
+class Clop(): # create class Clop():
+    pass # an empty block
+  
+A = Clop # appropriation
+A = 9 * 9 # your figures
+B = 9 * 9 # your figures
+print(A * B) # use function print()
