4a2c96f1b2ce0553cfcba0d55bb3e214d7ed0b1c
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-16 03:04:03 +0300

    Create class_Eco.py

---
+++ class_Eco.py
@@ -0,0 +1,12 @@
+# __Author__ __Lencof__
+# class_Eco.py
+
+class Eco(): # create class Eco():
+      pass # an empty block
+  
+A = Eco # appropriation
+A = (9 * 9 * 9 * 9) # your figures
+B = (9 * 9 * 9 * 9) # your figures
+print(A + B) # use function print()
+
+Eco() # close
