170e91422ebecac938a1f4d2c76c6c2f395a4d27
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-17 02:30:31 +0300

    Create class_Lobit.py

---
+++ class_Lobit.py
@@ -0,0 +1,13 @@
+# __Author__ __Lencof__
+# class_Lobit.py
+
+class Lobit: # create class Lobit
+    pass # an empty block
+  
+A = Lobit # appropriation 
+A = 1900 # your figures
+B = 9000 # your figures
+C = 8000 # your figures
+D = 2000 # your figures
+
+print(A + B + C + D) # use function print()
