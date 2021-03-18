81beb88321569c227faec7a5a17173ee8b26db03
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-18 14:16:12 +0300

    Create class_Pube.py

---
+++ class_Pube.py
@@ -0,0 +1,13 @@
+# __Author__ __Lencof__
+# class_Pube.py
+
+class Pube(): # create class Pube():
+    pass # an empty block
+  
+A = Pube # appropriation
+V = 10000 # your figures
+ASADA = 2000 # your figures
+BuGA = 1000 # your figures
+A = 1 # your figures
+
+print() # use function print()
