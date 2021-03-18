ea8659e5aeabefa3d98387a129afeada1b6c0285
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-18 14:11:29 +0300

    Create class_Ron.py

---
+++ class_Ron.py
@@ -0,0 +1,11 @@
+# __Author__ __Lencof__
+# class_Ron.py
+
+class Ron(): # create class Ron():
+    pass # an empty block
+  
+A = Ron # appropriation
+A = 1000 # your figures
+B = 2000 # your figures
+
+print(A + B) # use function print()
