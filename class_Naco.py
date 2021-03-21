13e4af19371d378206cd3414a90a501b5128cbde
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-21 06:56:54 +0300

    Update class_Naco.py

--- class_Naco.py
+++ class_Naco.py
@@ -1,1 +1,9 @@
+# __Author__ __Lencof__
+# class_Naco.py
 
+class Naco(): # create class Naco():
+  pass # empty block
+
+a = 500 # your figures
+b = 500 # your figures
+print(a + b) # print() deduces
