f0a1a959ba6d06fda243e848da54112733b97e14
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-29 08:21:49 +0300

    Create class_Polr.py

---
+++ class_Polr.py
@@ -0,0 +1,15 @@
+# __Author__ __Lencof__
+# class_Polr.py
+
+class Polr(): # create class Polr():
+    pass # an empty block
+  
+SA = Polr() # assignment 
+SA = 90 # your figures
+DAG = 700 # your figures
+
+def Anam(): # create def Anam
+   pass # an empty block
+  
+Polr() # Close
+Anam() # Close
