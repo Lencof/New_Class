f4ceb13aef8309d05563bd0a82be4906e7ee8989
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-31 07:39:41 +0300

    Create class_Breyt.py

---
+++ class_Breyt.py
@@ -0,0 +1,10 @@
+# __Author__ __Lencof__
+# class_Breyt.py
+
+class Breyt(): # create class Breyt()
+    pass # an empty block
+  
+D = Breyt # appropriation
+D = 1200 # your figures
+S = 300 # your figures
+print(D + S) # use print()
