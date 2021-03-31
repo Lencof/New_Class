83202475492cbdd6e1da70b222f6baaba702925b
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-31 07:37:28 +0300

    Create class_Bets.py

---
+++ class_Bets.py
@@ -0,0 +1,9 @@
+# __Author__ __Lencof__
+# class_Bets.py
+
+class Bets(): # create class Bets():
+    pass # an empty block
+  
+S = Bets # appropriation
+S = 5000 # your figures
+V = 5000 # your figures
