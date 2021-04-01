1e6fa45a025d3798283549d3b66643fcf3d1cbf4
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-01 10:39:08 +0300

    Create class_Botinx.py

---
+++ class_Botinx.py
@@ -0,0 +1,10 @@
+# __Author__ __Lencof__
+# class_Botinx.py
+
+class Botinx: # create class Botinx
+    pass # an empty block
+  
+X = Botinx # appropriation
+X = 30000 # your figures
+BAZ = 12000 # your figures
+print(X + BAZ) # use function print()
