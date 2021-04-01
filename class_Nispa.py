a048e744ce673688c72d69092dc58090045ebfde
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-01 10:35:28 +0300

    Create class_Nispa.py

---
+++ class_Nispa.py
@@ -0,0 +1,9 @@
+# __Author__ __Lencof__
+# class_Nispa.py
+
+class Nispa(): # create class Nispa():
+    pass # an empty block
+  
+QEA = Nispa # appropriation
+QEA = 1200 # your figures
+CAV = 1200 # your figures
