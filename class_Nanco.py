5d853b1f0f974722dc1011b4f0e73f8343a38639
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-02 14:38:23 +0300

    Create class_Nanco.py

---
+++ class_Nanco.py
@@ -0,0 +1,10 @@
+# __Author__ __Lencof__
+# class_Nanco.py
+
+class Nanco(): # create class Nanco():
+    pass # an empty block
+
+A = Nanco # appropriation
+A = 9 * 9 * 9 * 9 * 9 * 9 * 9 * 9 # your figures
+B = 9 # your figures
+print(A * B) # use function print()
