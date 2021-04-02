2110503058aaacfa322aef1da92225f8bd3fb297
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-02 14:30:56 +0300

    Create class_Bing.py

---
+++ class_Bing.py
@@ -0,0 +1,10 @@
+# __Author__ __Lencof__
+# class_Bing.py
+
+class Bing(): # create class Bing():
+    pass # An empty block
+  
+R = Bing # appropriation
+R = 1000 * 1000 * 1000
+AS = 3
+print(AS * R) # use function print()
