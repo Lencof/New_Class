d319572c703a78a7442f0c97252f1cebf8e332ae
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-06 03:19:28 +0300

    Create class_Bopox.py

---
+++ class_Bopox.py
@@ -0,0 +1,11 @@
+# __Author__ __Lencof__
+# class_Bopox.py
+
+# create class Bopox():
+class Bopox():
+    pass # an empty block
+  
+A = Bopox # appropriation
+A = 3 # your figures
+B = 3 # your figures
+print(A * B) # use function print()
