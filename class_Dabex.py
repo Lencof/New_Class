92065f2cbdde2848b5199123f81b308f4d4c38ae
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-11 10:01:07 +0300

    Create class_Dabex.py

---
+++ class_Dabex.py
@@ -0,0 +1,11 @@
+# __Author__ __Lencof__
+# class_Dabex.py
+
+class Dabex(): # create class Dabex():
+    pass # an enpty block
+  
+A = Dabex # appropriation
+A = 1200 # your figures
+B = 1200 # your figures
+
+print(A + B) # use function print()
