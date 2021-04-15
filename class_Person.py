7ec0fd78bd5a2df3f9763eadca71d2fed169bc30
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-15 04:38:25 +0300

    Update class_Person.py

--- class_Person.py
+++ class_Person.py
@@ -1,7 +1,12 @@
 # __Author__ __Lencof__ and __Swaroop__
 # encoding:utf-8
 #!/usr/bin/python
-class Person:
+
+import os
+import sys 
+
+# create class Person():
+class Person():
 	pass # An empty block
 
 p = Person()
