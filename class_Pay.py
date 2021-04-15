fa2fdbcbf4d48713f01d2eccbf59f5e5e45fb0d5
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-15 04:37:32 +0300

    Update class_Pay.py

--- class_Pay.py
+++ class_Pay.py
@@ -1,6 +1,9 @@
 # __Author__ __Lencof__
 # class_Pay.py
 
+import os
+import sys
+
 class Pay(): # create class Pay():
     print('You payments?') # your text
     print('You payments?') # your text
