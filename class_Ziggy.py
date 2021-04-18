e02732960bcbe14a5f0111b293c21c38246557a7
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-18 07:49:46 +0300

    Create class_Ziggy.py

---
+++ class_Ziggy.py
@@ -0,0 +1,12 @@
+# __Author__ __Lencof__
+# class_Ziggy.py
+
+# create class Ziggy():
+class Ziggy():
+    pass # an empty block
+  
+Q = Ziggy # appropriation
+Q = 999 # your figures
+C = 1000 # your figures
+
+print(Q + C) # use function print()
