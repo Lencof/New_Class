e73bc701c6302d0c8d6e82291864097f1a890e2c
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-18 07:52:07 +0300

    Create class_Spave.py

---
+++ class_Spave.py
@@ -0,0 +1,12 @@
+# __Author__ __Lencof__
+# class_Spave.py
+
+# create class Spave():
+class Spave():
+    pass # an empty block
+  
+A = Spave # appropriation
+S = 1000 # your figures
+A = 1000 # your figures
+
+print(A + S) # use function print()
