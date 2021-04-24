21b4c491162ad244a871a57fcc19922cbfe45d4e
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-24 12:52:31 +0300

    Create class_Clox.py

---
+++ class_Clox.py
@@ -0,0 +1,14 @@
+# __Author__ __Lencof__
+# class_Clox.py
+
+import os
+import sys
+
+# create class Bopox():
+class Clox():
+    pass # an empty block
+  
+A = Clox # appropriation
+A = 3 # your figures
+B = 3 # your figures
+print(A * B) # use function print()
