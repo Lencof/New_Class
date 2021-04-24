b6b4a602ff0d7f367f25abbd20cbc20ecc6891ec
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-24 12:51:20 +0300

    Create class_Bass.py

---
+++ class_Bass.py
@@ -0,0 +1,12 @@
+# __Author__ __Lencof__
+# class_Bass.py
+
+import os
+import sys
+
+class Bass(): # create class Bass():
+  pass # empty block
+
+a = 5 # your figures
+b = 5 # your figures
+print(a + b) # print() deduces
