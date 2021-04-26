0dc0d52710ee67f1d830d14c70833c7dea649e8b
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-26 04:05:05 +0300

    Create class_Niburu.py

---
+++ class_Niburu.py
@@ -0,0 +1,14 @@
+# __Author__ __Lencof__
+# class_Niburu.py
+
+import os
+import sys
+
+# create class Niburu
+class Niburu():
+    pass # An empty block
+  
+A = Niburu # appropriated
+A = 1000 # your figures
+V = 1000 # your figures
+print(A + V) # use function print()
