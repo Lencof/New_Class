b426bc6c1c0f9f837c7037764099aa2d568a23f0
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-28 03:03:53 +0300

    Create class_Macos.py

---
+++ class_Macos.py
@@ -0,0 +1,15 @@
+# __Author__ __Lencof__
+# class_Macos.py
+
+import os
+import sys
+
+# create class Macos
+class Macos():
+    pass # an empty block
+  
+Q = Macos # appropriation
+Q = 8000 # your figures
+B = 5 # your figures
+
+print(Q * B) # use function print()
