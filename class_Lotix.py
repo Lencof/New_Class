e141a1cd30f29068eecedcf4887a9804717d21fc
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-28 03:00:33 +0300

    Create class_Lotix.py

---
+++ class_Lotix.py
@@ -0,0 +1,14 @@
+# __Author__ __Lencof__
+# class_Lotix.py
+
+import os
+import sys 
+
+# create class_Lotix
+class Lotix():
+    pass # an empty block
+  
+X = Lotix # appropriation
+X = 1800 # your figures
+C = 1800 # your figures
+print(X + C) # use function print()
