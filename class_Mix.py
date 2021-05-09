925b0990d4bfb8e172530242c111c3d50003e5ae
Lencof <71154457+Lencof@users.noreply.github.com>
2021-05-09 09:42:23 +0300

    Create class_Mix.py

---
+++ class_Mix.py
@@ -0,0 +1,44 @@
+# __Author__ __Lencof__
+# class_Mix.py
+
+import os
+import sys
+
+# create class Mix
+class Mix():
+    print('Mix') # your text
+    print('Mix') # your text 
+    print('Mix') # your text 
+    
+    print('Mix') # your text 
+    print('Mix') # your text 
+    print('Mix') # your text 
+    
+    print('Mix') # your text 
+    print('Mix') # your text 
+    print('Mix') # your text 
+    
+    print('Mix') # your text
+    print('Mix') # your text 
+    print('Mix') # your text 
+    
+    print('Mix') # your text 
+    print('Mix') # your text 
+    print('Mix') # your text 
+    
+    print('Mix') # your text 
+    print('Mix') # your text 
+    print('Mix') # your text 
+    
+    print('Mix') # your text
+    print('Mix') # your text 
+    print('Mix') # your text 
+    
+    print('Mix') # your text 
+    print('Mix') # your text 
+    print('Mix') # your text 
+    
+    print('Mix') # your text 
+    print('Mix') # your text 
+    print('Mix') # your text 
+    
