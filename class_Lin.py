8141de31b72afba617bb7fe0d05f7cc146244bc7
Lencof <71154457+Lencof@users.noreply.github.com>
2021-05-09 09:45:21 +0300

    Create class_Lin.py

---
+++ class_Lin.py
@@ -0,0 +1,45 @@
+# __Author__ __Lencof__
+# class_Lin.py
+
+import os
+import sys
+
+# create class Lin
+class Lin():
+    print('Lin') # your text
+    print('Lin') # your text
+    print('Lin') # your text
+    
+    print('Lin') # your text
+    print('Lin') # your text
+    print('Lin') # your text
+    
+    print('Lin') # your text
+    print('Lin') # your text
+    print('Lin') # your text
+    
+    print('Lin') # your text
+    print('Lin') # your text
+    print('Lin') # your text
+    
+    print('Lin') # your text
+    print('Lin') # your text
+    print('Lin') # your text
+    
+    print('Lin') # your text
+    print('Lin') # your text
+    print('Lin') # your text
+    
+    print('Lin') # your text
+    print('Lin') # your text
+    print('Lin') # your text
+    
+    print('Lin') # your text
+    print('Lin') # your text
+    print('Lin') # your text
+    
+    print('Lin') # your text
+    print('Lin') # your text
+    print('Lin') # your text
+    
+Lin() # close
