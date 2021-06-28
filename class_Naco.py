818ce606cd80933b4bf1d4b02846755e05107259
Lencof <71154457+Lencof@users.noreply.github.com>
2021-06-28 16:03:42 +0300

    Update class_Naco.py

--- class_Naco.py
+++ class_Naco.py
@@ -1,11 +1,8 @@
 # __Author__ __Lencof__
 # class_Naco.py
 
-import os
-import sys 
-
 class Naco(): # create class Naco():
-  pass # empty block
+    pass # empty block
 
 a = 500 # your figures
 b = 500 # your figures
