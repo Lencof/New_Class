18e47939ad497ce2c0ad5414757f391a25f5a877
Lencof <71154457+Lencof@users.noreply.github.com>
2021-06-29 04:07:08 +0300

    Update class_Nom.py

--- class_Nom.py
+++ class_Nom.py
@@ -1,11 +1,8 @@
 # __Author__ __Lencof__
 # class_Nom.py
 
-import os
-import sys 
-
 class Nom(): # create class A():
-  pass # empty block
+    pass # empty block
 
 a = 1001 # your figures
 b = 1001 # your figures
