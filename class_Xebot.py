0f2be0904820d4bcb5863c9d987fe732f37c6f1f
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-01 04:55:11 +0300

    Update class_Xebot.py

--- class_Xebot.py
+++ class_Xebot.py
@@ -7,7 +7,7 @@ class Xebot():
   
 X = Xebot # appropriation
 VE = (9 * 9 * 9 * 9) # your figures
-XE = (4 * 4 * 2)
+XE = (4 * 4 * 2) # your figures 
 print(VE * XE) # use function print()
 
 Xebot() # close
