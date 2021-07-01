b1c2e29b7a20abc5eff73ec878d67d3778b3f6f5
Lencof <71154457+Lencof@users.noreply.github.com>
2021-07-01 04:54:44 +0300

    Create class_Xebot.py

---
+++ class_Xebot.py
@@ -0,0 +1,13 @@
+# __Author__ __Lencof__
+# class_Xebot.py
+
+# create class Xebot():
+class Xebot():
+    pass # An empty block
+  
+X = Xebot # appropriation
+VE = (9 * 9 * 9 * 9) # your figures
+XE = (4 * 4 * 2)
+print(VE * XE) # use function print()
+
+Xebot() # close
