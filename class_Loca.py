a7e4fb01b3440e4f364d0afd2c962974a9be9514
Lencof <71154457+Lencof@users.noreply.github.com>
2022-01-14 12:50:18 +0600

    Update class_Loca.py

--- class_Loca.py
+++ class_Loca.py
@@ -1,9 +1,9 @@
 # __Author__ __Lencof__
 # class_Loca.py
 
-class Loca(): # create class Loca():
+class Loca(): 
     pass # an empty block
   
-a = 999 # your figures
-v = 999 # your figures
-print(a + v) # print() deduces
+a = 999 
+v = 999 
+print(a + v) 
